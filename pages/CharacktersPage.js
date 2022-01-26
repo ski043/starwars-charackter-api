ed684a071a2b65bf9277699d0f238584b35da4d2
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2022-01-26 22:31:03 +0100

    page created

---
+++ pages/CharacktersPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CharacktersPage = ({ name, image }) => {
+  return (
+    <div>
+      <h1 className="text-white">{name}</h1>
+    </div>
+  );
+};
+
+export default CharacktersPage;
