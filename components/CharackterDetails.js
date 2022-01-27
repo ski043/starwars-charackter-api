05088093ed76b953fffd04f666f47e60155a0996
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2022-01-27 22:38:20 +0100

    updated learn more

--- components/CharackterDetails.js
+++ components/CharackterDetails.js
@@ -1,6 +1,7 @@
+import Link from "next/link";
 import React from "react";
 
-const CharackterDetails = ({ name, image, height, mass, gender }) => {
+const CharackterDetails = ({ name, image, height, mass, gender, wiki }) => {
   return (
     <div className="flex flex-col lg:flex-row lg:items-center">
       <div className="lg:w-1/2">
@@ -17,9 +18,13 @@ const CharackterDetails = ({ name, image, height, mass, gender }) => {
         <p className="text-xl text-gray-300 pt-10">Height: {height}</p>
         <p className="text-xl text-gray-300 pt-2">Mass: {mass}</p>
         <p className="text-xl text-gray-300 pt-2">Gender: {gender}</p>
-        <button className="bg-zinc-700 py-4 px-6 hover:bg-red-500 duration-200 rounded-lg w-full text-xl font-semibold text-white mt-10 lg:w-1/2">
-          Learn More!
-        </button>
+        <Link href={wiki}>
+          <a>
+            <button className="bg-zinc-700 py-4 px-6 hover:bg-red-500 duration-200 rounded-lg w-full text-xl font-semibold text-white mt-10 lg:w-1/2">
+              Learn More!
+            </button>
+          </a>
+        </Link>
       </div>
     </div>
   );
